c7b563fe1c749f3d63544f42dcfa6c8da202c814
JaneFromAux <post.fuer.j.schaefer@gmail.com>
2022-04-14 14:52:21 +0200

    fertig

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+
+function math(a, b) {
+    console.log(a * b);
+    console.log(a / b);
+}
+
+math(10, 2);
+console.log("Nächste Aufgabe:");
+math(30, 20);
+console.log("Nächste Aufgabe:");
+math(100, 100);
+console.log("Nächste Aufgabe:");
+math(5, 0);
+console.log("Nächste Aufgabe:");
+math(45, 173);
+console.log("Nächste Aufgabe:");
+math(1, 1000);
+console.log("Aufgaben erledigt");
